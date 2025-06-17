e23074e80d4583955a5fb330f10be1a0cd71db00
GALIH RIDHO UTOMO <92840324+4211421036@users.noreply.github.com>
2025-06-18 02:09:05 +0700

    Update gui.py

--- laigui/gui.py
+++ laigui/gui.py
@@ -5,6 +5,24 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtCore import Qt
 from pqcrypto import keygen, encrypt, decrypt
 
+from PyQt5.QtCore import QThread, pyqtSignal
+
+class EncryptWorker(QThread):
+    finished = pyqtSignal(object)
+
+    def __init__(self, pk, msg, k, p, a, P0):
+        super().__init__()
+        self.pk = pk
+        self.msg = msg
+        self.k = k
+        self.p = p
+        self.a = a
+        self.P0 = P0
+
+    def run(self):
+        from pqcrypto import encrypt
+        ct = encrypt(self.pk, self.msg, self.k, self.p, self.a, self.P0)
+        self.finished.emit(ct)
 
 class LAIGUIApp(QWidget):
     def __init__(self):
@@ -39,8 +57,13 @@ class LAIGUIApp(QWidget):
         self.initUI()
         self.setLayout(self.layout)
 
+        self.p = 10007
+        self.a = 5
+        self.P0 = (1, 0)
+        self.k = 7
+
         # Generate keypair
-        self.pk, self.sk = keygen()
+        self.pk, self.sk = keygen(self.p, self.a, self.P0)
 
     def initUI(self):
         self.plainText = QLineEdit()
@@ -68,16 +91,32 @@ class LAIGUIApp(QWidget):
 
     def encryptMessage(self):
         msg = self.plainText.text().encode()
-        ct = encrypt(self.pk, msg)
-        self.encryptedText.setText(str(ct))
+        self.encryptButton.setEnabled(False)
+        self.encryptButton.setText("Encrypting...")
 
-        # Store for decryption
+        self.worker = EncryptWorker(self.pk, msg, self.k, self.p, self.a, self.P0)
+        self.worker.finished.connect(self.onEncryptionDone)
+        self.worker.start()
+
+    def onEncryptionDone(self, ct):
         self.last_cipher = ct
+        self.encryptedText.setText(str(ct))
+        self.encryptButton.setEnabled(True)
+        self.encryptButton.setText("Encrypt")
+
 
     def decryptMessage(self):
         try:
-            pt = decrypt(self.sk, self.last_cipher)
+            pt = decrypt(self.sk, self.last_cipher, self.k, self.p, self.a, self.P0)
             self.decryptedText.setText(pt.decode())
         except Exception as e:
             self.decryptedText.setText("Error during decryption: " + str(e))
 
+if __name__ == "__main__":
+    import sys
+    from PyQt5.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    window = LAIGUIApp()
+    window.show()
+    sys.exit(app.exec_())
